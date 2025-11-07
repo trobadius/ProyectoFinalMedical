f414271ebb53c041b011c85243dbc60a90196684
mateogarzon90@gmail.com <mateogarzon90@gmail.com>
2025-11-07 19:43:01 +0100

    prueba de colores en App.Css

--- mediaccion/src/App.jsx
+++ mediaccion/src/App.jsx
@@ -5,9 +5,11 @@ import './App.css'
 function App() {
 
   return (
-    
+      <div>
      <h1>Bienvenido a MediAccion</h1>
-    
+      <h2>Tu plataforma de salud amigable</h2>
+
+    </div>
   )
 }
 
