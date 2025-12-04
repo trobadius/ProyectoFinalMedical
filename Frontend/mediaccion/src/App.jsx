4996550471a9fe3aa2f6240777289df56ea16fc1
hao <haochen718@gmail.com>
2025-12-04 19:01:01 +0100

    arreglo componente

--- Frontend/mediaccion/src/App.jsx
+++ Frontend/mediaccion/src/App.jsx
@@ -6,7 +6,6 @@ import Calendario from './pages/Calendario.jsx';
 import Perfil from './pages/Perfil.jsx';
 import Login from './pages/Login.jsx';
 import Progresos from "./pages/Progresos.jsx";
-import ProgressBar from './components/ProgressBar.jsx';
 import Logout from './components/Logout.jsx';
 import NotFound from './pages/NotFound.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
@@ -28,34 +27,6 @@ export default function App() {
         minHeight: '100vh'
       }}
     >
-
-      {/* CONTENEDOR GLOBAL QUE IGUALA ANCHOS Y HACE TODO RESPONSIVE */}
-      {/*
-      <main className="w-full max-w-screen-lg mx-auto px-0 py-6">
-        <Routes>
-          <Route element={<ProtectedRoute/>} >
-            <Route element={<NavbarOutlet/>} >
-                <Route path="/" element={<Home />} />
-                <Route path="/calendario" element={<Calendario />} />
-                <Route path="/tesseractOCR" element={<TesseractOCR />} />
-                <Route path="/perfil" element={<Perfil />} />
-                <Route path="/Progresos1" element={<Progresos1 />} />
-                <Route path="/Progresos2" element={<Progresos2 />} />
-                <Route path="/Progresos3" element={<Progresos3 />} />
-                <Route path="/chatbot" element={<Chatbot />} />
-                <Route path="/Escanerqr" element={<Escanerqr/>} />
-
-            </Route>
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />}/>
-          <Route path="/register" element={<Logout metodo="register" />} />
-          <Route path="/registration" element={<Register />} />
-          <Route path="*" element={<NotFound />}/>
-          <Route path="/Chatbot" element={<Chatbot />} />
-        </Routes>
-      </main>
-      */}
       <main className="w-full max-w-screen-lg mx-auto px-0 py-6">
         <Routes>
           <Route element={<ProtectedRoute />} >
@@ -66,9 +37,6 @@ export default function App() {
               <Route path="/tesseractOCR" element={<TesseractOCR />} />
               <Route path="/perfil" element={<Perfil />} />
               <Route path="/progresos" element={<Progresos />} />
-              <Route path="/openai" element={<OpenAiApi />} />
-              <Route path="/zxingScanner" element={<ZxingScanner />} />
-
             </Route>
           </Route>
           <Route path="/login" element={<Login />} />
