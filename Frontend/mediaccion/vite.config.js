80618398a5ab56676799cc5a16c3095ebec74e0a
hao <haochen718@gmail.com>
2025-12-15 21:14:56 +0100

    configuracion docker

--- Frontend/mediaccion/vite.config.js
+++ Frontend/mediaccion/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//  base: process.env.VITE_BASE_PATH || '/ProyectoFinalMedical',
 export default defineConfig({
   plugins: [react()],
 })
